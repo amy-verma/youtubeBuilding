fa702290d404bfd7a5521aabe4360a3ecda74a3a
amy-verma <amitjsr8786@gmail.com>
2025-08-04 12:33:49 +0530

    Debouncing

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -1,12 +1,35 @@
 import { useDispatch } from "react-redux"
 import { toggleMenu } from "../utils/appSlice"
+import { useEffect, useState } from "react"
+import { YOUTUBE_SEARCH_API } from "../utils/constants"
 
 const Head=()=>{
+    const [searchQuery,setSearchQuery]=useState("")
     const dispatch=useDispatch()
 
     const toggleMenuHandler=()=>{
          dispatch(toggleMenu())
     }
+
+    useEffect(()=>{
+        //API CAll
+        console.log(searchQuery);
+
+        getSearchSuggestion()
+        //Make an api call after every key press
+        //but if the difference betwwen the two pi call is <200
+        //decline the api call
+
+    },[])
+
+    const getSearchSuggestion=async()=>{
+        const data=await fetch(YOUTUBE_SEARCH_API+searchQuery);
+        const response=await data.json()
+        console.log(response);
+
+    }
+
+    
     return (
         <div className="grid grid-flow-col p-5 m-2 shadow-lg">
             <div className="flex col-span-1 " >
@@ -14,7 +37,7 @@ const Head=()=>{
                 <a href="/"><img className="h-8 mx-2" alt="youtube" src="https://static.vecteezy.com/system/resources/previews/011/998/173/non_2x/youtube-icon-free-vector.jpg"/></a> 
             </div>
             <div className="col-span-10 px-10">
-                <input className="w-1/2 border border-gray-400 p-2 rounded-l-full"  type="text" placeholder="Search Here" />
+                <input value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} className="w-1/2 border border-gray-400 p-2 rounded-l-full"  type="text" placeholder="Search Here" />
                 <button className="border border-gray-400  px-5 py-2 rounded-r-full bg-gray-100 ">Search</button>
             </div>
             <div className="col-span-1">
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -2,3 +2,4 @@ const GOOGLE_API_KEY="AIzaSyABXeV_C5Mskk4zixIVFttEYlA6DjuPcNo"
 
 export const YOUTUBE_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 
+export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
