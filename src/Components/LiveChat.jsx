cc718986d6178b5d88feb0621678e09c7b022ea7
amy-verma <amitjsr8786@gmail.com>
2025-08-28 12:19:38 +0530

    live-chat-code-with-random-name

--- src/Components/LiveChat.jsx
+++ src/Components/LiveChat.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
+import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat=()=>{
 
@@ -15,11 +16,11 @@ const LiveChat=()=>{
         console.log("API Polling")
 
         dispatch(addMessage({
-            name:"Akshay Saini",
-            message:"Lorem Ipsum Dolor Site 🚀"
+            name:generateRandomName(),
+            message:makeRandomMessage(20) + "🚀"
         }))
 
-        },2000)
+        },500)
          return()=>{
 
          clearInterval(i)
@@ -27,8 +28,8 @@ const LiveChat=()=>{
     },[])
 
     return (
-        <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll">
-
+        <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+            
             {/* Don't use indexex as key */}
            {chatMessages.map((c,i)=><ChatMessage key={i}
             name={c.name}
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,7 @@
 const GOOGLE_API_KEY="AIzaSyABXeV_C5Mskk4zixIVFttEYlA6DjuPcNo"
 
+export const LIVE_CHAT_COUNT=25
+
 export const YOUTUBE_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 
 // export const YOUTUBE_SEARCH_API="https://developers.google.com/youtube/v3/docs/search/list"
