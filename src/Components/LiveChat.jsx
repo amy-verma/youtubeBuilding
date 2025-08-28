ff50e2ed61c6579e6f766adab8a13c04e8cf2f9e
amy-verma <amitjsr8786@gmail.com>
2025-08-28 13:51:31 +0530

    user add comment

--- src/Components/LiveChat.jsx
+++ src/Components/LiveChat.jsx
@@ -1,26 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat=()=>{
+    const [liveMessage,setLiveMessage]=useState("")
 
     const dispatch=useDispatch()
 
     const chatMessages=useSelector((store)=>store.chat.messages)
 
     useEffect(()=>{
+        
        const i= setInterval(()=>{
         //API Polling
-        console.log("API Polling")
+        // console.log("API Polling")
 
         dispatch(addMessage({
             name:generateRandomName(),
             message:makeRandomMessage(20) + "🚀"
         }))
 
-        },500)
+        },2000)
          return()=>{
 
          clearInterval(i)
@@ -28,14 +30,35 @@ const LiveChat=()=>{
     },[])
 
     return (
-        <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
-            
-            {/* Don't use indexex as key */}
-           {chatMessages.map((c,i)=><ChatMessage key={i}
-            name={c.name}
-             message={c.message}
-             />)}
-         </div>
+        <>
+    <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+            <div>
+                    {/* Don't use indexex as key */}
+                {chatMessages.map((c,i)=><ChatMessage key={i}
+                name={c.name}
+                message={c.message}
+                />)}
+            </div>
+    </div>
+
+        <form 
+        className="border border-black w-full p-2 ml-2"
+         onSubmit={(e)=>{
+            e.preventDefault()
+            // console.log("ON fROm Submit",liveMessage)
+            dispatch(addMessage({
+                name:"Amit Verma",
+                message:liveMessage
+            }))
+            setLiveMessage("")
+         }
+        }
+            >
+            <input className="w-96 px-2" type="text" value={liveMessage} onChange={(e)=>setLiveMessage(e.target.value)}/>
+            <button className="px-2 mx-2 bg-green-300">Send</button>
+        </form>
+          
+         </>
     )
 }
 export default LiveChat;
